bb6c6759377268bc246e6a76dd49e8812e75e818
Anna Burlot <anna.burlot@gmail.com>
2022-12-03 15:23:49 +0100

    Second step

--- 03/index.ts
+++ 03/index.ts
@@ -29,8 +29,36 @@ const getSumOfPriorities = (input: string): number => {
     return misplacedItem ? getPriorityByLetter(misplacedItem) : 0;
   });
 
-  // calculate priority of that letter
   return misplacedItemsPriorities.reduce((prev, curr) => prev + curr);
 };
 
+const getSumOfPrioritiesOfBadges = (input: string): number => {
+  const badgeByGroup = input.split('\n');
+
+  let groups: string[][][] = [];
+
+  for (let i = 0; i < badgeByGroup.length; i += 3) {
+    const group = badgeByGroup.slice(i, i + 3).map((elf) => elf.split(''));
+    groups = [...groups, group];
+  }
+
+  const groupsBadgeLetters = groups
+    .map((group) => {
+      const groupBadgeLetter = group.reduce((a, b) =>
+        a.filter((c) => b.includes(c))
+      );
+      const uniqueGroupBadgeLetter = groupBadgeLetter.filter(
+        (item, index) => groupBadgeLetter.indexOf(item) === index
+      );
+      return uniqueGroupBadgeLetter;
+    })
+    .flat();
+
+  return groupsBadgeLetters
+    .map((letter) => getPriorityByLetter(letter))
+    .reduce((prev, curr) => prev + curr);
+};
+
 console.log(getSumOfPriorities(input));
+console.log(getSumOfPrioritiesOfBadges(input));
+console.log(getSumOfPrioritiesOfBadges(testInput) === 70);
